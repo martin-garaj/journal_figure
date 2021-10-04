d7dc29115f070ddba1615d4d629004de24a6d2b3
martin3366 <garaj.martin@gmail.com>
2021-10-04 12:02:13 +0800

    README update

--- journal_figure/__init__.py
+++ journal_figure/__init__.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from journal_figure.library_package import *
+
+
+#%% ---------------------------------------------------------------------------
+#   ---------------------------------FUNCTIONS---------------------------------
+#   ---------------------------------------------------------------------------
+
+from journal_figure.library_package import pretty_detail_axis
+from journal_figure.library_package import set_style
+from journal_figure.library_package import set_ticks
+from journal_figure.library_package import prettify_minor_ticks
+from journal_figure.library_package import prettify_major_ticks
+from journal_figure.library_package import set_figure_size
+from journal_figure.library_package import _latexify
+from journal_figure.library_package import pretty_legend
+
